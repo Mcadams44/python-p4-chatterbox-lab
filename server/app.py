5433bf04b9d686d5adc3bf0c642e3986ccf8b83f
Mcadams <mcadamsproject@gmail.com>
2025-09-16 11:20:48 +0300

    chatterbox1

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
-from flask_migrate import Migrate
 
 from models import db, Message
 
@@ -10,17 +9,39 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 CORS(app)
-migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        return jsonify([message.to_dict() for message in messages])
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+        db.session.add(message)
+        db.session.commit()
+        return jsonify(message.to_dict()), 201
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get_or_404(id)
+    
+    if request.method == 'PATCH':
+        data = request.get_json()
+        message.body = data['body']
+        db.session.commit()
+        return jsonify(message.to_dict())
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return '', 204
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=4000, debug=True)
